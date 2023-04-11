beeb1b209f5def5e7dde3f425fbc77060494d819
hinkel <markus.hinkel@tuwien.ac.at>
2023-04-11 13:40:04 +0200

    fix: fix wrong exec format

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+#See https://docs.gitlab.com/runner/executors/custom.html#cleanup
+
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${currentDir}/base.sh # Get variables from base.
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#See https://docs.gitlab.com/runner/executors/custom.html#run
 
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${currentDir}/base.sh # Get variables from base.
